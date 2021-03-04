c6c05ab5f65019a6c42f214ebfeea36f342cd9e2
Johnny Kung <kjohnathan.engi@gmail.com>
2021-03-03 22:31:22 -0800

    db config changed

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,9 +4,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
+	user: "b2ac729a0afb5b",
+	password: "99054de0",
+	database: "heroku_45ef60956338eea",
 	multipleStatements: false
 };
 
